5fd666dda7992b3977964eb553324b0d2275462f
Brian <bstanton773@gmail.com>
2023-03-21 15:36:48 -0500

    Redirect to home page after sign up

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from fake_data import posts
 from app.forms import SignUpForm
 
@@ -8,9 +8,21 @@ def index():
     return render_template('index.html', posts=posts, logged_in=False)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+    # Create an instance of the form (in the context of the current request)
     form = SignUpForm()
+    # Check if the form was submitted and that all of the fields are valid
+    if form.validate_on_submit():
+        # If so, get the data from the form fields
+        print('Hooray our form was validated!!')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
+        return redirect(url_for('index'))
     return render_template('signup.html', form=form)
 
 
