fe11c1e24aa0aa6f56185e1bc8670e0d88b9cc57
Brian <bstanton773@gmail.com>
2023-03-21 15:55:38 -0500

    Add Message Flashing on signup

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from fake_data import posts
 from app.forms import SignUpForm
 
@@ -22,6 +22,7 @@ def signup():
         username = form.username.data
         password = form.password.data
         print(first_name, last_name, email, username, password)
+        flash(f"Thank you {first_name} for signing up!", "success")
         return redirect(url_for('index'))
     return render_template('signup.html', form=form)
 
