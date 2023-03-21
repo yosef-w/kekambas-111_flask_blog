f144914cf5c55a0a375cc20d34bd44c82ebe6fdb
Brian <bstanton773@gmail.com>
2023-03-21 14:05:34 -0500

    Add fake data for posts, display all posts on home page

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template
+from fake_data import posts
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts, logged_in=False)
 
 
 @app.route('/signup')
