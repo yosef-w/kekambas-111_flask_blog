4be5c928931e7803c8d004ddd0fa292cd04ccb9c
yosef-w <ywolday24@gmail.com>
2023-04-05 15:36:40 -0500

    Add to_dict method to Post. Add routes for getting all posts and single post

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,19 @@
 from . import api
+from app.models import Post
 
 
 @api.route('/')
 def index():
     return 'Hello this is the API'
+
+@api.route('/posts')
+def get_posts():
+    posts = Post.query.all()
+    return [post.to_dict() for post in posts]
+
+@api.route('/posts/<post_id>')
+def get_post(post_id):
+    post = Post.query.get(post_id)
+    if post is None:
+        return {'error': f'Post with the ID of {post_id} is not found'}, 404
+    return post.to_dict()
