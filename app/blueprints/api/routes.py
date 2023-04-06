37a1003d7fd77b183b9c1806585ef258f1202bf5
yosef-w <ywolday24@gmail.com>
2023-04-06 13:23:42 -0500

    Update routes and models

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,4 +1,5 @@
 from flask import request
+from app import db
 from . import api
 from app.models import Post, User
 
@@ -7,16 +8,18 @@ from app.models import Post, User
 def index():
     return 'Hello this is the API'
 
-@api.route('/posts')
+# Endpoint to get all of the posts
+@api.route('/posts', methods=["GET"])
 def get_posts():
     posts = Post.query.all()
     return [post.to_dict() for post in posts]
 
-@api.route('/posts/<post_id>')
+# Endpoint to get a single post by ID
+@api.route('/posts/<int:post_id>')
 def get_post(post_id):
     post = Post.query.get(post_id)
     if post is None:
-        return {'error': f'Post with the ID of {post_id} is not found'}, 400
+        return {'error': f'Post with the ID of {post_id} does not exist.'}, 404
     return post.to_dict()
 
 # Endpoint to create a new post
@@ -36,25 +39,55 @@ def create_post():
             missing_fields.append(field)
     if missing_fields:
         return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
-    
-    return 'This is the Create Post Route'
 
-@api.route('/signup')
-def signup():
+    # Get the data from the request body
+    title = data.get('title')
+    body = data.get('body')
+    image_url = data.get('image_url')
+    user_id = data.get('user_id')
+
+    # Create a new Post instance with the data from the request
+    new_post = Post(title=title, body=body, image_url=image_url, user_id=user_id)
+
+    # Return the new post as a JSON response
+    return new_post.to_dict(), 201
+
+
+# Endpoint to get a user by their ID
+@api.route('/users/<user_id>', methods=["GET"])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return {'error': f'User with the ID of {user_id} does not exist.'}, 404
+    return user.to_dict()
+
+
+# Endpoint to create a new user
+@api.route('/users', methods=["POST"])
+def create_user():
+    # Check to see that the request body is JSON aka application/json content-type
     if not request.is_json:
         return {'error': 'Your request content-type must be application/json'}, 400
+    # Get the data from the request body
     data = request.json
-    required_fields = ['first_name', 'last_name', 'email', 'username', 'password', 'confirm_pass']
+    # Validate that all of the required fields are present
+    required_fields = ['first_name', 'last_name', 'email', 'username', 'password']
     missing_fields = []
     for field in required_fields:
         if field not in data:
             missing_fields.append(field)
     if missing_fields:
         return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
-    
-@api.route('/signup/<user_id>')
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user is None:
-        return {'error': f'{user.id} is not found.'}, 400
-    return user.to_user()
+    # Get the data from the request body
+    first = data.get('first_name')
+    last = data.get('last_name')
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+    # Check to see if there is already a user with that username and/or email
+    check_user = db.session.execute(db.select(User).filter((User.username == username) | (User.email == email))).scalars().all()
+    if check_user:
+        return {'error': 'User with that username and/or email already exists'}, 400
+    # Create a new user instance with the request data
+    new_user = User(first_name=first, last_name=last, email=email, username=username, password=password)
+    return new_user.to_dict(), 201
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+import base64
+import os
 from random import randint
 from app import db, login
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -14,6 +16,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -26,14 +30,31 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
-    
-    def to_user(self):
+
+    def to_dict(self):
         return {
+            'id': self.id,
             'first_name': self.first_name,
             'last_name': self.last_name,
             'email': self.email,
-            'password': self.password,
+            'username': self.username,
+            'date_created': self.date_created,
         }
+    
+    def get_token(self, expires_in=300):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.commit()
+        return self.token
+
+    def revoke_token(self):
+        now = datetime.utcnow()
+        self.token_expiration = now - timedelta(seconds=1)
+        db.session.commit()
+
 
 @login.user_loader
 def get_a_user_by_id(user_id):
@@ -58,7 +79,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.id}|{self.title}>"
-    
+
     def to_dict(self):
         return {
             'id': self.id,
